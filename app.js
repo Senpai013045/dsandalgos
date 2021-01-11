c44dbcd748294d2ef1d656b83c485b541d8f701e
Senpai013045 <Subham.hero01@gmail.com>
2021-01-11 09:40:24 +0300

    recursion challenges complete

--- app.js
+++ app.js
@@ -213,3 +213,110 @@ console.log(
   "Factorial of 5 using recursive function is: ",
   recursiveFactorial(5)
 );
+
+//helper method recursion
+
+function collectOddValues(arr) {
+  let result = [];
+
+  function helper(helperInput) {
+    if (helperInput.length === 0) return;
+
+    if (helperInput[0] % 2 !== 0) {
+      result.push(helperInput[0]);
+    }
+    //slice returns a new array with elements starting from given index and not including the second index given(this is optional for copying all values except the one before certain index)
+    helper(helperInput.slice(1));
+  }
+
+  helper(arr);
+
+  return result;
+}
+
+console.log(
+  "The odd values found using helper method recursion: ",
+  collectOddValues([1, 2, 3, 4, 5, 6, 7])
+);
+
+//pure recursive way
+
+function collectOddValuesThePureRecursionWay(arr) {
+  //this collects the values returned but remember that it will reset every function call
+  let collector = [];
+
+  //is input is empty array, break out of this recursion
+  if (arr.length === 0) {
+    return collector;
+  }
+
+  //checking if the first number is odd and pushing it to collector
+  if (arr[0] % 2 !== 0) {
+    collector.push(arr[0]);
+  }
+
+  //now,the second important thing in recursion is to call the same function with different input but making sure the input hits the base case
+  //we have to use something that wont mutate the input ( so .splice(),.push(),.pop() are a no no!!!)
+  return collector.concat(collectOddValuesThePureRecursionWay(arr.slice(1)));
+  //.concat() returns a new array, so does .slice()
+}
+
+console.log(
+  "Odd values collected through pure recursion: ",
+  collectOddValuesThePureRecursionWay([1, 2, 3])
+);
+
+//Visual represantation
+
+/*
+  collectOddValuesThePureRecursionWay([1,2,3]);
+
+  is input's length 0? > no!
+
+  is the first value odd > yes!
+
+  collector=[1] //since we pushed the first value
+
+  collector.concat(collectOffValuesThePureRecursionWay([2,3]))
+
+  [1].concat(collectOffValuesThePureRecursionWay([2,3]))
+              is input empty? > no!
+              is 2 odd? > no!
+              collection in this instance is [] //empty
+              [].concat(collectOffValuesThePureRecursionWay([3])
+                        is input empty? > no
+                        is 3 odd? > yes
+                        collector in this instance is [3]
+                        [3].concat(collectOffValuesThePureRecursionWay([])
+                                    is input empty? >yes
+                                    return []
+
+  final calculation : [1].concat([].concat([3].concat([])))      
+  >[1,3]                        
+  
+
+  */
+
+//challenge problem
+
+function power(base, exponent) {
+  if (exponent === 0) return 1;
+
+  return base * power(base, exponent - 1);
+}
+
+console.log("The base of 3 and exponent of 3 results: ", power(3, 3));
+
+//product of array challenge
+
+function productOfArray(arr) {
+  let collector = 0;
+
+  if (arr.length === 0) return collector;
+
+  collector += arr[0];
+
+  return collector + productOfArray(arr.slice(1));
+}
+
+console.log("the product of [1,2,3,4,5] is: ", productOfArray([1, 2, 3, 4, 5]));
